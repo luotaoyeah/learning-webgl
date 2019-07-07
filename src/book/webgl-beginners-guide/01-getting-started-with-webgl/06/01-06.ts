e53177aefde5ed8e20cb649671ef4b9ca54997ba
luotao <luotaoyeah@gmail.com>
2019-07-07 09:01:31 +0800

    feat(webgl-beginners-guide): 01-06: HTMLCanvasElement.getContext()

---
+++ src/book/webgl-beginners-guide/01-getting-started-with-webgl/06/01-06.ts
@@ -0,0 +1,12 @@
+window.addEventListener("load", function() {
+  const canvasEl = document.querySelector<HTMLCanvasElement>("#canvas01");
+  if (canvasEl) {
+    /*
+     * 使用 HTMLCanvasElement.getContext() 方法, 获取 WebGL 的上下文对象
+     */
+    const gl = canvasEl.getContext("webgl");
+    if (gl) {
+      console.log("gl instanceof WebGLRenderingContext", gl instanceof WebGLRenderingContext);
+    }
+  }
+});
