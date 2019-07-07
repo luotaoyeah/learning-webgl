63d4a1a4078071ef0ba55efb7ac37ba34d2ddab7
luotao <luotaoyeah@gmail.com>
2019-07-07 12:01:53 +0800

    feat(webgl-beginners-guide): 01-10

---
+++ src/book/webgl-beginners-guide/01-getting-started-with-webgl/10/01-10.ts
@@ -0,0 +1,7 @@
+import { init } from "../../00/00.js";
+
+init().then((gl: WebGLRenderingContext) => {
+  /*
+   * 本书的源码地址: https://bitbucket.org/dcantor/webgl-beginners-guide-code/src/master/
+   */
+});
