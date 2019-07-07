6845ba63c2f61226138f4c5d17623f4a2a6b594d
luotao <luotaoyeah@gmail.com>
2019-07-07 18:11:41 +0800

    feat(webgl-beginners-guide): 02-05: WebGLRenderingContextBase.drawElements()

--- src/book/webgl-beginners-guide/02-rendering-geometry/03/02-03.ts
+++ src/book/webgl-beginners-guide/02-rendering-geometry/03/02-03.ts
@@ -1,73 +1,16 @@
 import { getShader, initGL } from "../../00/00.js";
 
 initGL().then((gl: WebGLRenderingContext) => {
-  initProgram(gl);
-
-  /*
-   * 构造 VBO/IBO 的步骤如下:
-   *
-   * 1. 构造 vertex 数据的数组
-   */
-  const vertexArray = getVertexArray();
-  /* 2. 使用 WebGLRenderingContextBase.createBuffer() 方法, 创建一个 buffer 对象 */
-  const VBO = gl.createBuffer();
-  /* 3. 使用 WebGLRenderingContextBase.bindBuffer() 方法, 绑定 current buffer */
-  gl.bindBuffer(gl.ARRAY_BUFFER, VBO);
-  /* 4. 使用 WebGLRenderingContextBase.bufferData() 方法, 设置 buffer 数据 */
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexArray), gl.STATIC_DRAW);
-  /* 5. 使用 WebGLRenderingContextBase.bindBuffer() 方法, 解绑 current buffer, 传递的参数为 null */
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-  const indexArray = getIndexArray();
-  const IBO = gl.createBuffer();
-  /*
-   * 对于 WebGLRenderingContextBase.bindBuffer() 和 WebGLRenderingContextBase.bufferData() 方法,
-   * 对于 vertex 来说, 第一个参数为 WebGLRenderingContextBase.ARRAY_BUFFER
-   * 对于 index 来说, 第一个参数为 WebGLRenderingContextBase.ELEMENT_ARRAY_BUFFER
-   */
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, IBO);
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexArray), gl.STATIC_DRAW);
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+  const program = initProgram(gl);
+  const [, indexArray, VBO, IBO] = initBuffers(gl);
+  render(gl, program, indexArray, VBO, IBO);
 });
 
-/*
- * 对于顶点(vertex)来说, 每 3 个元素一组, 分别表示一个顶点的 x/y/z 坐标, 每一组都有一个索引(index), 从 0 开始
+/**
+ *
+ * @param gl
  */
-function getVertexArray(): Array<number> {
-  return [
-    ...[1.5, 0, 0],
-    ...[-1.5, 1, 0],
-    ...[-1.5, 0.809017, 0.587785],
-    ...[-1.5, 0.309017, 0.951057],
-    ...[-1.5, -0.309017, 0.951057],
-    ...[-1.5, -0.809017, 0.587785],
-    ...[-1.5, -1, 0],
-    ...[-1.5, -0.809017, -0.587785],
-    ...[-1.5, -0.309017, -0.951057],
-    ...[-1.5, 0.309017, -0.951057],
-    ...[-1.5, 0.809017, -0.587785],
-  ];
-}
-
-/*
- * 对于 index 来说, 每 3 个元素一组, 组成一个三角形的三个顶点
- */
-function getIndexArray(): Array<number> {
-  return [
-    ...[0, 1, 2],
-    ...[0, 2, 3],
-    ...[0, 3, 4],
-    ...[0, 4, 5],
-    ...[0, 5, 6],
-    ...[0, 6, 7],
-    ...[0, 7, 8],
-    ...[0, 8, 9],
-    ...[0, 9, 10],
-    ...[0, 10, 1],
-  ];
-}
-
-function initProgram(gl: WebGLRenderingContext) {
+function initProgram(gl: WebGLRenderingContext): WebGLProgram {
   let vertexShader = getShader(gl, "shader-vs");
   let fragmentShader = getShader(gl, "shader-fs");
 
@@ -86,5 +29,107 @@ function initProgram(gl: WebGLRenderingContext) {
 
     // @ts-ignore
     program.aVertexPosition = gl.getAttribLocation(program, "aVertexPosition");
+  } else {
+    throw new Error("COULD NOT INITIALISE SHADERS");
   }
+
+  return program as WebGLProgram;
+}
+
+/**
+ *
+ * @param gl
+ */
+function initBuffers(gl: WebGLRenderingContext): [Array<number>, Array<number>, WebGLBuffer, WebGLBuffer] {
+  /*
+   * 构造 VBO/IBO 的步骤如下:
+   */
+
+  /*
+   * 1. 构造 vertex 数据的数组
+   * 对于顶点(vertex)来说, 每 3 个元素一组, 分别表示一个顶点的 x/y/z 坐标, 每一组都有一个索引(index), 从 0 开始
+   */
+  const vertexArray = [...[-0.5, 0.5, 0.0], ...[-0.5, -0.5, 0.0], ...[0.5, -0.5, 0.0], ...[0.5, 0.5, 0.0]];
+  /* 2. 使用 WebGLRenderingContextBase.createBuffer() 方法, 创建一个 buffer 对象 */
+  const VBO = gl.createBuffer();
+  if (!VBO) {
+    throw new Error("VBO IS NULL");
+  }
+  /* 3. 使用 WebGLRenderingContextBase.bindBuffer() 方法, 绑定 current buffer */
+  gl.bindBuffer(gl.ARRAY_BUFFER, VBO);
+  /* 4. 使用 WebGLRenderingContextBase.bufferData() 方法, 设置 buffer 数据 */
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexArray), gl.STATIC_DRAW);
+  /* 5. 使用 WebGLRenderingContextBase.bindBuffer() 方法, 解绑 current buffer, 传递的参数为 null */
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+  /*
+   * 对于 index 来说, 每 3 个元素一组, 组成一个三角形的三个顶点
+   */
+  const indexArray = [...[3, 2, 1], ...[3, 1, 0]];
+  const IBO = gl.createBuffer();
+  if (!IBO) {
+    throw new Error("IBO IS NULL");
+  }
+
+  /*
+   * 对于 WebGLRenderingContextBase.bindBuffer() 和 WebGLRenderingContextBase.bufferData() 方法,
+   * 对于 vertex 来说, 第一个参数为 WebGLRenderingContextBase.ARRAY_BUFFER
+   * 对于 index 来说, 第一个参数为 WebGLRenderingContextBase.ELEMENT_ARRAY_BUFFER
+   */
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, IBO);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexArray), gl.STATIC_DRAW);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+  return [vertexArray, indexArray, VBO, IBO];
+}
+
+/**
+ *
+ * @param gl
+ * @param program
+ * @param indexArray
+ * @param VBO
+ * @param IBO
+ */
+function render(
+  gl: WebGLRenderingContext,
+  program: WebGLProgram,
+  indexArray: Array<number>,
+  VBO: WebGLBuffer,
+  IBO: WebGLBuffer,
+) {
+  // @ts-ignore
+  window.requestAnimationFrame(render.bind(this, gl, program, indexArray, VBO, IBO));
+  drawScene(gl, program, indexArray, VBO, IBO);
+}
+
+/**
+ *
+ * @param gl
+ * @param program
+ * @param indexArray
+ * @param VBO
+ * @param IBO
+ */
+function drawScene(
+  gl: WebGLRenderingContext,
+  program: WebGLProgram,
+  indexArray: Array<number>,
+  VBO: WebGLBuffer,
+  IBO: WebGLBuffer,
+) {
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.enable(gl.DEPTH_TEST);
+
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.viewport(0, 0, 1280, 720);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, VBO);
+  // @ts-ignore
+  gl.vertexAttribPointer(program.aVertexPosition, 3, gl.FLOAT, false, 0, 0);
+  // @ts-ignore
+  gl.enableVertexAttribArray(program.vertexPosition);
+
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, IBO);
+  gl.drawElements(gl.TRIANGLES, indexArray.length, gl.UNSIGNED_SHORT, 0);
 }
