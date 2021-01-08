315080b507e61e368986eaf19469158c76457a9a
luotao <luotaoyeah@gmail.com>
2021-01-08 21:34:48 +0800

    feat(learn-threejs): 01.05.adding-materials-lights-and-shadows

--- src/book/learn-threejs/01.creating-your-first-3-d-scene-with-three-js/04.rendering-and-viewing-a-3-d-object/02-first-scene.ts
+++ src/book/learn-threejs/01.creating-your-first-3-d-scene-with-three-js/04.rendering-and-viewing-a-3-d-object/02-first-scene.ts
@@ -1,41 +1,41 @@
 // @ts-ignore
 function init() {
   // create a scene, that will hold all our elements such as objects, cameras and lights.
-  var scene = new THREE.Scene();
+  const scene = new THREE.Scene();
 
   // create a camera, which defines where we're looking at.
-  var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+  const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
   // create a render and set the size
-  var renderer = new THREE.WebGLRenderer();
+  const renderer = new THREE.WebGLRenderer();
   renderer.setClearColor(new THREE.Color(0x000000));
   renderer.setSize(window.innerWidth, window.innerHeight);
 
   // show axes in the screen
-  var axes = new THREE.AxesHelper(20);
+  const axes = new THREE.AxesHelper(20);
   scene.add(axes);
 
   // create the ground plane
-  var planeGeometry = new THREE.PlaneGeometry(60, 20);
-  var planeMaterial = new THREE.MeshBasicMaterial({
+  const planeGeometry = new THREE.PlaneGeometry(60, 20);
+  const planeMaterial = new THREE.MeshBasicMaterial({
     color: 0xaaaaaa,
   });
-  var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
   // rotate and position the plane
-  plane.rotation.x = -0.5 * Math.PI;
+  plane.rotation.x = -((90 * Math.PI) / 180);
   plane.position.set(15, 0, 0);
 
   // add the plane to the scene
   scene.add(plane);
 
   // create a cube
-  var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
-  var cubeMaterial = new THREE.MeshBasicMaterial({
+  const cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
+  const cubeMaterial = new THREE.MeshBasicMaterial({
     color: 0xff0000,
     wireframe: true,
   });
-  var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
   // position the cube
   cube.position.set(-4, 3, 0);
@@ -44,12 +44,12 @@ function init() {
   scene.add(cube);
 
   // create a sphere
-  var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
-  var sphereMaterial = new THREE.MeshBasicMaterial({
+  const sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
+  const sphereMaterial = new THREE.MeshBasicMaterial({
     color: 0x7777ff,
     wireframe: true,
   });
-  var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 
   // position the sphere
   sphere.position.set(20, 4, 2);
