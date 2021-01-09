336157367f2454584d310db72dc324dcd9a191e4
luotao <luotaoyeah@gmail.com>
2021-01-09 16:55:28 +0800

    chore(learn-threejs): type definitions for threejs geometries/utils/controls

--- src/book/learn-threejs/types.d.ts
+++ src/book/learn-threejs/types.d.ts
@@ -1,7 +1,24 @@
-import * as THREE from 'three';
+import * as three from 'three';
 import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls';
+import { ConvexGeometry as OriginalConvexGeometry } from 'three/examples/jsm/geometries/ConvexGeometry';
+import { ParametricGeometry as OriginalParametricGeometry } from 'three/examples/jsm/geometries/ParametricGeometry';
+import { ParametricGeometries as OriginalParametricGeometries } from 'three/examples/jsm/geometries/ParametricGeometries';
+import { SceneUtils as OriginalSceneUtils } from 'three/examples/jsm/utils/SceneUtils';
+import { ConvexHull as OriginalConvexHull } from 'three/examples/jsm/math/ConvexHull';
 
 declare global {
+  export namespace THREE {
+    export class ConvexGeometry extends OriginalConvexGeometry {}
+
+    export class ParametricGeometry extends OriginalParametricGeometry {}
+
+    export class ConvexHull extends OriginalConvexHull {}
+
+    export const SceneUtils: typeof OriginalSceneUtils;
+    export const ParametricGeometries: typeof OriginalParametricGeometries;
+  }
+
   export function initStats(type?: string | number): Stats;
-  export function initTrackballControls(camera: THREE.Camera, renderer: THREE.WebGLRenderer): TrackballControls;
+
+  export function initTrackballControls(camera: three.Camera, renderer: three.WebGLRenderer): TrackballControls;
 }
